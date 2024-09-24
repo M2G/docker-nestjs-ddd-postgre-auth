8d4802cf267d320dcc39402f534c830059307c8d
mapierrelouis <matthieu.pierrelouis@cpexterne.org>
2024-09-24 15:11:26 +0200

    feat: fix impl di

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import AppController from './app.controller';
 import AppService from './app.service';
 import { UsersController } from '@application/controllers/users/users.controllers';
 import { UserService } from '@domain/services/users/user.service';
+import UserModule from './user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  controllers: [AppController, UsersController],
-  imports: [DatabaseModule],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'test_db',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  providers: [AppService],
 })
 export default class AppModule {}
--- src/domain/services/users/user.service.ts
+++ src/domain/services/users/user.service.ts
@@ -1,16 +1,13 @@
 import { Injectable, Inject } from '@nestjs/common';
-import User from '@domain/entities/user';
 import CreateUserDto from '@application/dto/users/user.dto';
 import UserRepository from '@infrastructure/repository/user/user.repository';
 import { InjectModel } from '@nestjs/sequelize';
+import User from '@infrastructure/repository/user';
 
 // Se inyecta el repo en el servicio
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectModel(User)
-    private readonly userRepository: typeof UserRepository,
-  ) {}
+  constructor(private readonly repository: UserRepository) {}
 
   /*
   getHello(): string {
@@ -21,8 +18,7 @@ export class UserService {
     return await this.repository.create(createUserDto);
   }
 */
-  find({ ...args }: { args: any }): Promise<User[]> {
-    //@ts-ignore
-    return this.userRepository.find({ ...args });
+  find({ ...args }: { args: any }): Promise<any[]> {
+    return this.repository.find({ ...args } as any);
   }
 }
