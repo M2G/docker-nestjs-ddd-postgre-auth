81cdc0ff956ca1418354135d275d33af4b1c2184
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-12-16 02:10:56 +0100

    feat: add import/export index domain service

---
+++ src/domain/services/index.ts
@@ -0,0 +1,7 @@
+import TokenService from './token';
+import UserService from './users';
+import AuthService from './auth';
+import YcI18nService from './yc-i18n';
+import RedisService from './cache';
+
+export { AuthService, TokenService, UserService, YcI18nService, RedisService };
