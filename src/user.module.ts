8e686a9356a88286061d2128ed2135b4001c335f
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-12-16 02:30:24 +0100

    feat: add user module part

--- src/user.module.ts
+++ src/user.module.ts
@@ -1,19 +1,37 @@
 import { Module } from '@nestjs/common';
 import UsersService from '@domain/services/users';
-import { UsersController } from '@application/controllers/users/users.controllers';
+import { UserControllers } from '@application/controllers';
 import { SequelizeModule } from '@nestjs/sequelize';
-import User from './infrastructure/models/user.model';
-//import { JwtStrategy } from '../auth/strategy/jwt.strategy';
-import UserRepository from '@infrastructure/repository/user/user.repository';
+import { RedisRepository, UsersRepository } from '@infrastructure/repository';
+import User from '@infrastructure/models/user/user.model';
+import { LocalStrategy } from '@application/auth/strategies/local.strategy';
+import { JwtStrategy } from '@application/auth/strategies/jwt.strategy';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { RedisService } from '@domain/services';
+import redisConfig from 'src/config';
+import { ConfigModule } from '@nestjs/config';
+import redisClientFactory from './config';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
-  controllers: [UsersController],
+  controllers: [UserControllers],
+  exports: [UsersService, UsersRepository, RedisService, RedisRepository],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    /*JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),*/
+  ],
   providers: [
     UsersService,
-    UserRepository,
-    // JwtStrategy
+    UsersRepository,
+    RedisService,
+    RedisRepository,
+    // JwtStrategy,
+    //UsersRepository,
+    JwtService,
+    //LocalStrategy,
+    redisClientFactory,
   ],
-  exports: [UsersService],
 })
 export default class UsersModule {}
