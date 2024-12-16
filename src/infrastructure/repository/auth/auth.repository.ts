bc6a8b99cbfe14911c9c74878660bc832146a4b9
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-12-16 02:20:21 +0100

    feat: add infrastructure repository auth part

--- src/infrastructure/repository/auth/auth.repository.ts
+++ src/infrastructure/repository/auth/auth.repository.ts
@@ -1,41 +1,84 @@
+import { Injectable, Inject, forwardRef, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { JwtService } from '@nestjs/jwt';
 import { UniqueConstraintError, Op } from 'sequelize';
-import { Injectable } from '@nestjs/common';
-import CreateUserDto from '@application/dto/users';
-import User from '@infrastructure/models/user.model';
+import * as bcrypt from 'bcrypt';
+import { User } from '@infrastructure/models';
+import { encryptPassword, validatePassword } from '@encryption';
+import { CreateUserDto, UpdateUserDto } from '@application/dto/users';
+import updateUserDto from '@application/dto/users/update-user.dto';
+import { RedisService } from '@domain/services';
+import config from '@config';
+
+export interface IAuthRepository {}
 
 @Injectable()
-class UsersService {
+class AuthRepository implements IAuthRepository {
   constructor(
     @InjectModel(User)
     private readonly userModel: typeof User,
+    private readonly jwtService: JwtService,
+
+    @Inject(forwardRef(() => RedisService))
+    private readonly redisService: RedisService,
   ) {}
 
-  async authenticate({ email }: { email: string }): Promise<unknown | null> {
-    try {
-      const user = await this.userModel.findOne(
-        { where: { email } },
-        // , { raw: true }
-      );
+  login(user: User): { accessToken: string } {
+    const payload = { email: user.email, id: user.id };
+    const options = { secret: config.authSecret };
+    return { accessToken: this.jwtService.sign(payload, { secret: config.authSecret }) };
+  }
 
-      console.log('authenticate', user);
-      if (!user) return null;
+  async validateUser({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<User | null> {
+    try {
+      const user = await this.userModel.findOne({ raw: true, where: { email } });
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
+      const isMatch: boolean = bcrypt.compareSync(password, user.password);
+      if (!isMatch) {
+        throw new BadRequestException('Password does not match');
+      }
       return user;
     } catch (error) {
       throw new Error(error as string | undefined);
     }
   }
 
-  /*
-    findOne(id: number): Promise<User> {}
-    changePassword(): Promise<User[]> {}
-    forgotPassword(): Promise<User[]> {}
-    resetPassword(): Promise<User[]> {}
-    register(): Promise<User[]> {}
-    authenticate(): Promise<User[]> {}
-    async update(id: number, updateUserDto: any): Promise<User[]> {}
-
-     */
+  async register({
+    created_at,
+    deleted_at,
+    email,
+    password,
+  }: CreateUserDto): Promise<{ accessToken: string }> {
+    try {
+      const hashPassword = encryptPassword(password);
+      const user = (await this.userModel.create(
+        {
+          created_at,
+          deleted_at,
+          email,
+          password: hashPassword,
+        },
+        { raw: true },
+      )) as any;
+      return this.login({
+        email,
+        id: user.id,
+      } as User);
+    } catch (error) {
+      if (error instanceof UniqueConstraintError) {
+        throw new Error('Duplicate error');
+      }
+      throw new Error(error as string | undefined);
+    }
+  }
 }
 
-export default UsersService;
+export default AuthRepository;
