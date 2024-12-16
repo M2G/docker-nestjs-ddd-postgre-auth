4cf991449f15298a43658aa222ac0301b76afc35
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-12-16 02:26:37 +0100

    feat: add app module  part

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,72 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, forwardRef } from '@nestjs/common';
 // import { DatabaseModule } from '@infrastructure/database/database.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
+import LoggerMiddleware from '@application/middleware';
+import { YcI18nModule } from './yc-i18n/yc-i18n.module';
 import AppController from './app.controller';
 import AppService from './app.service';
 // import { UsersController } from '@application/controllers/users/users.controllers';
 // import { UserService } from '@domain/services/users/user.service';
-import UserModule from './user.module';
-import LoggerMiddleware from '@application/middleware';
+import UsersModule from './user.module';
+import RedisModule from './redis.module';
+import { OrdersModule } from './test/order.module';
+import { PaymentModule } from './test/payment.module';
+import { UserService } from '@domain/services';
+import UserRepository from '@infrastructure/repository/user/user.repository';
+import User from '@infrastructure/models/user';
+import { LocalStrategy } from '@application/auth/strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import AuthModule from './auth.module';
+//import { RedisService } from './config/redis';
 
 @Module({
   controllers: [AppController],
   imports: [
+    RedisModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
     SequelizeModule.forRoot({
+      autoLoadModels: true,
+      database: 'test_db',
       dialect: 'postgres',
       host: 'postgres',
-      port: 5432,
-      username: 'postgres',
       password: 'postgres',
-      database: 'test_db',
-      autoLoadModels: true,
+      port: 5432,
       synchronize: true,
+      username: 'postgres',
     }),
-    UserModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.resolve(__dirname, '../../src/locales/'),
+        watch: true,
+      },
+      resolvers: [
+        new QueryResolver(['lang']),
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
+      // typesOutputPath: path.join(__dirname, '../src/generated/i18n.generated.ts'),
+    }),
+    YcI18nModule,
+    OrdersModule,
+    PaymentModule,
+    UsersModule,
+    AuthModule,
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // LocalStrategy,
+    // UserRepository,
+    // ProductRepo,
+    // ProductService,
+    // UserService
+  ],
+  exports: [],
 })
 export default class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
