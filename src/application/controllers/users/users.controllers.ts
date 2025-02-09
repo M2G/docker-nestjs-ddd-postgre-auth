2fd2ddf489a46d89d8ec3a75e1cba4512c3d6b73
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-02-09 04:20:32 +0100

    feat: refactor application controllers users part

--- src/application/controllers/users/users.controllers.ts
+++ src/application/controllers/users/users.controllers.ts
@@ -18,10 +18,12 @@ import {
   HttpException,
   HttpStatus,
   NotFoundException,
+  HttpCode,
 } from '@nestjs/common';
 import UserService from '@domain/services/users';
 import { YcI18nService } from '@domain/services';
 import { LocalAuthGuard } from '@application/auth/guards/local.guard';
+import { UserEntity as User } from '@domain/entities';
 // import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
@@ -33,32 +35,32 @@ class UsersController {
   ) {}
 
   @Get()
-  async findAll(@Request() req, @Res() response): Promise<any> {
-    const { ...args } = req.query;
-    const users = await this.usersService.find(args);
-    return response.status(HttpStatus.OK).send(users);
+  findAll(@Request() { query: { ...args } }): Promise<User[]> {
+    console.log('args', args);
+    return this.usersService.find(args as any) as any;
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<any> {
+  async findOne(@Param('id') id: string): Promise<User | null> {
     const user = await this.usersService.findOne({ id: Number(id) });
     if (!user) {
       throw new NotFoundException(
         this.i18n.t('users.notFound', {
           args: { id },
-        }) as string,
+        }),
       );
     }
-    return user;
+    return user as unknown as User;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body(new ValidationPipe()) updateUserDto): any {
-    return this.usersService.update({ id: Number(id), ...updateUserDto });
+  update(@Param('id') id: string, @Body(new ValidationPipe()) updateUserDto): User | null {
+    return this.usersService.update({ id: Number(id), ...updateUserDto }) as any;
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string, @Res() response): Promise<any> {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string): Promise<boolean> {
     const user = await this.usersService.remove({ id: Number(id) });
     if (!user) {
       throw new NotFoundException(
@@ -67,12 +69,25 @@ class UsersController {
         }) as string,
       );
     }
-    response.status(HttpStatus.NO_CONTENT).send();
+    return !!user;
   }
 
   @Post('register')
   create(@Body(new ValidationPipe()) createUserDto): any {
     return this.usersService.register(createUserDto);
+  }
+
+  @Post('forgot-password')
+  forgotPassword(@Body(new ValidationPipe()) forgotPassword): any {
+    return this.usersService.forgotPassword(forgotPassword);
+  }
+
+  @Post('reset-password')
+  resetPassword(@Body(new ValidationPipe()) { password }, @Request() { query: { token } }): any {
+    return this.usersService.resetPassword({
+      password,
+      reset_password_token: token,
+    });
   }
 
   /*
