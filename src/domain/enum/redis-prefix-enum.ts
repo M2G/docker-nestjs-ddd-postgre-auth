35f5d67b5284713770d40e0b80050a44c931ce51
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-02-09 04:22:53 +0100

    feat: code refactor domain enum

--- src/domain/enum/redis-prefix-enum.ts
+++ src/domain/enum/redis-prefix-enum.ts
@@ -1,7 +1,8 @@
 enum RedisPrefixEnum {
   USER = 'user',
-  USERS = 'usesr',
+  USERS = 'users',
   RESET_TOKEN = 'reset_token',
+  LAST_CONNECTED_AT = 'LAST_CONNECTED_AT:*',
 }
 
 export default RedisPrefixEnum;
