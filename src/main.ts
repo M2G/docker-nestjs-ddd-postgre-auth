aecedce31aa1c856756b5fa23dae067dc8f8632e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-08-04 02:12:56 +0200

    feat: add interface nest fastify

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import {
 import AppModule from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
     new FastifyAdapter(),
-
     { cors: true });
   await app.listen(8181, '0.0.0.0');
 }
