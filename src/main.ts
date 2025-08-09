643e4a829307c2a62adea5ddff8fb8626409bee4
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-08-09 15:45:28 +0200

    feat: add conf cors (put medthod)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import type {
+  NestFastifyApplication} from '@nestjs/platform-fastify';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+  FastifyAdapter
 } from '@nestjs/platform-fastify';
+import type { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import type { FastifyCorsOptions } from '@fastify/cors';
 import AppModule from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-    { cors: true });
+
+const CorsOptions: FastifyCorsOptions = {
+  origin: [
+    // ? Development environment
+    "http://localhost:3002",
+    // * Add Production and Staging URLs here
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
+  maxAge: 86400,
+};
+
+async function bootstrap(): Promise<void> {
+  const FastifyModule = new FastifyAdapter({ logger: true });
+
+  FastifyModule.enableCors(CorsOptions);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, FastifyModule);
   await app.listen(8181, '0.0.0.0');
 }
 bootstrap();
